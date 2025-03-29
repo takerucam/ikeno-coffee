31d878fe9d138e5e24ea868f36b16635c7519760
Ikeno Sosuke <ikenosousuke@icloud.com>
2025-03-29 14:07:43 +0900

    よくわからん

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("hello vr");
+
+AFRAME.registerComponent('foo', {
+  init: function () {
+    // Set up throttling.
+    this.throttledFunction = AFRAME.utils.throttle(this.everySecond, 1000, this);
+    AFRAME.utils.device.checkHeadsetConnected ()
+    
+  },
+
+  everySecond: function () {
+    // Called every second.
+    console.log("A second passed.");
+  },
+
+  tick: function (t, dt) {
+    this.throttledFunction();  // Called once a second.
+    console.log("A frame passed.");  // Called every frame.
+   },
+
+});
